0082aa4f69ddc28f30ea22d3a56e90ee52c6187e
Ze Feng Wang <zefengwang613@gmail.com>
2023-01-21 12:09:00 -0500

    BP Code

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+app = dash.Dash()
+
+if (__name__ == "__main__"){
+    app.run_server(debug=True)
+}
