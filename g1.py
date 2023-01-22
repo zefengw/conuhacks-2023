2e99e778e523e13e35976f12ce5669ec648161c8
Sacha Arseneault <sacha.arseneault@gmail.com>
2023-01-22 04:02:19 -0500

    final graph 1

--- g1.py
+++ g1.py
@@ -1,12 +1,14 @@
-import pandas as pd
+import json 
 from dash import dcc, html, dash
 from dash.dependencies import Input, Output
 import plotly.express as px
 from utils import *
 
 
+
+
 # --- DATA EXTRACTION ---
-import json 
+
 timestamp, symbol, price, market = [], [], [], []
 
 def parseFile(path: str) -> None:
@@ -38,12 +40,15 @@ def generateOptions():
     return options
 
     
-g1 = dash.Dash()
-g1.layout = html.Div([
+g1 = dash.Dash(__name__)
+g1.layout = html.Div(className="g1_container", children=[
 
-    html.H1("Graph 1", style={}),
+    html.H1("Graph 1", className="g1_h1"),
 
-    dcc.Dropdown(id="market_selected",
+    html.Div(className="g1_label", children=[
+        html.P(children=["Market: "], style={"color":"#ffffff", "margin": "10px"}),
+        dcc.Dropdown(id="market_selected",
+        className="dropdown",
                  options=[
                     {"label": "Aequitas", "value": "Aequitas"},
                     {"label": "Alpha", "value": "Alpha"},
@@ -51,22 +56,26 @@ g1.layout = html.Div([
                      ],
                  multi=False,
                  value="Aequitas",
-                 style={'width': "50%"}
-                 ),
-    dcc.Dropdown(id="symbol_selected",
+                 style={'width':"300px", "background-color": "#D5D5D5"}
+                 )]),
+    html.Div(className="g1_label", children=[
+        html.P(children=["Symbol: "], style={"color":"#ffffff", "margin": "10px"}),
+        dcc.Dropdown(id="symbol_selected",
+        className="dropdown",
                  options=generateOptions(),
                  multi=False,
                  value="All",
-                 style={'width': "50%"}
-                 ),
+                 style={'width':"300px", "background-color": "#D5D5D5"}
+                 )]),
     
 
     html.Br(),
     html.Br(),
 
-    dcc.Graph(id='graph1', figure={}, style={'height': '400px'})
+    dcc.Graph(id='graph1', figure={}, style={'height': '500px', "margin": "0px 30px 30px 30px"})
 
 ])
+
 
 
 @g1.callback(
@@ -118,11 +127,20 @@ def update_graph(m_selected, s_selected):
         display_dt.append(epoch_to_datetime(int(k)))
 
     if len(display_dt) == 0 or len(copy2_price) == 0:
-        fig = px.line(title="This stock is not traded in the selected market")
+        fig = px.area(title="This stock is not traded in the selected market")
     else:
-        fig = px.line(x=display_dt, y=copy2_price, render_mode='webgl')
+        fig = px.area(x=display_dt, y=copy2_price, title="Stock order requests over time",labels=dict(x="Time ", y="Price ($) "))
 
 
+    fig.update_layout(paper_bgcolor="#303030",
+    plot_bgcolor="#303030",
+    font_color="#919191",
+    margin_pad=30,
+    )
+    fig.layout.yaxis.gridcolor = "#4A4A4A"
+    fig.layout.yaxis.dividercolor = "#4A4A4A"
+    fig.layout.xaxis.gridcolor = "#4A4A4A"
+    fig.layout.xaxis.gridcolor = "#4A4A4A"
 
 
     return fig
