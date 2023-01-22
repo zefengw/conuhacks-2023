e3c5439f6b1763c4e3575c9efc641a7428456797
Ze Feng Wang <zefengwang613@gmail.com>
2023-01-22 02:20:54 -0500

    Fixed up 3.

--- app.py
+++ app.py
@@ -6,61 +6,14 @@ from collections import defaultdict
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 
-# obj = utils.load_json("TSXData.json")
-# num_intervals = 100
-
-# end = utils.END_EPOCH/(10**9)
-# start = utils.START_EPOCH/10**9
-# diff = (end - start) / num_intervals
-
-# df = pd.DataFrame(columns=["Interval", "Price", "Number of Orders"])
-# intervals = []
-# volume = defaultdict(int)
-# prices = defaultdict(list)
-
-# for i in range(1, num_intervals + 1):
-#     interval = str(start + i * diff)
-#     intervals.append(interval)
-
-# for i in range(len(obj)):
-#     if obj[i]["MessageType"] == "NewOrderRequest":
-#         for j in intervals:
-#             epoch_timestamp = int(obj[i]["TimeStampEpoch"]) / 10**9
-#             if epoch_timestamp < float(j):
-#                 volume[j] += 1
-#                 prices[j].append(obj[i]["OrderPrice"])
-
-#                 break
-
-# avg_prices = defaultdict(float)
-# for i in prices:
-#     avg_prices[i] = sum(prices[i]) / len(prices[i])
-
-# for i in intervals:
-#     price = avg_prices[i]
-#     num_orders = volume[i]
-#     date_obj = utils.epoch_seconds_to_datetime(int(float(i))).strftime("%H:%M:%S")
-#     new_row = pd.DataFrame(
-#         {"Interval": [date_obj], "Price": [price], "Number of Orders": [num_orders]})
-#     df = pd.concat([new_row, df.loc[:]]).reset_index(drop=True)
-
-# 3. volume = len(df[df["MessageType"] == "NewOrderRequest"])
-# start epoch (epoch at 09:28) / end epoch (epoch at 09:32), end - start / # of intervals = duration of intervals,
-# epoch(start) + (end-beg/n),
-
-# 4. store order ids
-# find average prices by appending prices of certain key intervals (k1, k2...) and dividing by # of elements in that interval
-
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1("Time/Volume/Price Graph"),
     dcc.Graph(id="time-volume-price-graph"),
-    # html.H3(id="time-volume-price-graph"),
     dcc.Dropdown(id="dropdown", options=[{'label': 'TSX', 'value': 'TSX'}, {'label': 'Aequitas', 'value': 'Aequitas'}, {
                  'label': 'Alpha', 'value': 'Alpha'}], value='TSX'),
 ])
-
 
 @app.callback(
     Output("time-volume-price-graph", "figure"),
